fe56fa01657137203db37f1a71a9c13a184ffda9
htl-leonding <t.stuetz@htl-leonding.ac.at>
2020-11-12 23:24:33 +0100

    lecture 2020-11-12

---
+++ my-first-project/src/main/java/at/htl/control/InitBean.java
@@ -0,0 +1,33 @@
+package at.htl.control;
+
+import io.quarkus.runtime.StartupEvent;
+import org.jboss.logging.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+@ApplicationScoped
+public class InitBean {
+
+    // private final Logger logger = Logger.getLogger(InitBean.class.getSimpleName())
+
+    @Inject
+    Logger LOGGER;
+
+    @Inject
+    GreetingService greetingService;
+
+    public InitBean() {
+    }
+
+    @PostConstruct
+    private void init() {
+        LOGGER.info(greetingService.greeting("Susi"));
+    }
+
+    void onStart(@Observes StartupEvent ev) {
+        LOGGER.error("The application is starting...");
+    }
+}
